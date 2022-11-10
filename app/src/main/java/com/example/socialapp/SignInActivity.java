7f4f18b64d801bfee7d031382b76ea9c1c81cbc5
priyansh-jain10 <54763105+priyansh-jain10@users.noreply.github.com>
2022-11-10 11:11:30 +0530

    Initial commit (Google Sign In)

---
+++ app/src/main/java/com/example/socialapp/SignInActivity.java
@@ -0,0 +1,107 @@
+package com.example.socialapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.CellSignalStrengthNr;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SignInActivity extends AppCompatActivity {
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient gsc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        SignInButton gsb=findViewById(R.id.googleSigninButton);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+         gsc= GoogleSignIn.getClient(this,gso);
+        gsb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1000){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                firebaseAuthwithGoogle(task.getResult(ApiException.class).getIdToken());
+            } catch (ApiException e) {
+                Log.d("SignInActivity",e.toString());
+                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    private void firebaseAuthwithGoogle(String idToken) {
+        AuthCredential credential= GoogleAuthProvider.getCredential(idToken,null);
+        FirebaseUser user=mAuth.getCurrentUser();
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user=mAuth.getCurrentUser();
+                    updateUi(user);
+                }
+                else{
+                    updateUi(null);
+                }
+            }
+        });
+    }
+    private void googleSignIn() {
+        Intent signInIntent=gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUi(currentUser);
+    }
+
+    private void updateUi(FirebaseUser firebaseUser) {
+        if(firebaseUser!=null){
+            startActivity(new Intent(SignInActivity.this,HomeScreen.class));
+            finish();
+
+        }
+        else{
+
+        }
+
+    }
+}
